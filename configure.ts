cfb48c015cb1aba26056b043e9a32fda26c609e4
jeremy <jeremy@chaufourier.fr>
2024-06-21 23:45:11 +0200

    feat: add configure

--- configure.ts
+++ configure.ts
@@ -5,9 +5,15 @@
  * @copyright Jeremy Chaufourier <jeremy@chaufourier.fr>
  */
 
-// import type Configure from '@adonisjs/core/commands/configure'
+import type Configure from '@adonisjs/core/commands/configure'
 
-export async function configure() {
-// export async function configure(command: Configure) {
-  // const codemods = await command.createCodemods()
+export async function configure(command: Configure) {
+  const codemods = await command.createCodemods()
+
+  /**
+   * Register provider
+   */
+  await codemods.updateRcFile((rcFile) => {
+    rcFile.addProvider('@jrmc/adonis-attachment/attachment_provider')
+  })
 }

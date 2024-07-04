4571e3749c2aba6bfd2b5a4f7ccbd191b4db4bd7
jeremy <jeremy@chaufourier.fr>
2024-07-05 01:01:21 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
+import attachmentManager from './services/main.js'
+
 export { configure } from './configure.js'
 export { Attachment } from './src/attachments/attachment.js'
 export { attachment } from './src/decorators/attachment.js'
 export { defineConfig } from './src/define_config.js'
 export { Attachmentable } from './src/mixins/attachmentable.js'
+export { attachmentManager }

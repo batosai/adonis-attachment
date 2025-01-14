69ee6b48664825ef40aeb0ecc33e7d06c42c2fb8
jeremy <jeremy@chaufourier.fr>
2025-01-15 00:28:41 +0100

    feat(test): detele attachment

---
+++ tests/attachment.spec.ts
@@ -0,0 +1,21 @@
+/**
+ * @jrmc/adonis-attachment
+ *
+ * @license MIT
+ * @copyright Jeremy Chaufourier <jeremy@chaufourier.fr>
+ */
+
+import { test } from '@japa/runner'
+import { UserFactory } from './fixtures/factories/user.js'
+
+test.group('attachment', () => {
+
+  test('delete', async ({ assert }) => {
+    const user = await UserFactory.create()
+    user.avatar = null
+    await user.save()
+
+    assert.isNull(user.avatar)
+  })
+})
+

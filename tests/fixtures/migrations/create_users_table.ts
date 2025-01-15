881158a3260ecc47f74c618d9b12842a680528dc
jeremy <jeremy@chaufourier.fr>
2025-01-16 00:38:35 +0100

    Create create_users_table.ts

---
+++ tests/fixtures/migrations/create_users_table.ts
@@ -0,0 +1,23 @@
+/**
+ * @jrmc/adonis-attachment
+ *
+ * @license MIT
+ * @copyright Jeremy Chaufourier <jeremy@chaufourier.fr>
+ */
+import { BaseSchema } from '@adonisjs/lucid/schema'
+export default class extends BaseSchema {
+  protected tableName = 'users'
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.json('avatar')
+      table.json('avatar_2')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

037836df419651ec811134935b1d8a22f7264d1f
jeremy <jeremy@chaufourier.fr>
2025-04-03 00:18:17 +0200

    remove: attachmentable

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ export { Attachment } from './src/attachments/attachment.js'
 export { attachment } from './src/decorators/attachment.js'
 export { attachments } from './src/decorators/attachment.js'
 export { defineConfig } from './src/define_config.js'
-export { Attachmentable } from './src/mixins/attachmentable.js'
 export * as errors from './src/errors.js'
 export { attachmentManager }
 export { RegenerateService }

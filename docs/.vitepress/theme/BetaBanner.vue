9d4d135c5579bb022bea7bf8ed3fa2ac45d8f629
Jeremy <jeremy@chaufourier.fr>
2026-09-26 11:23:52 +0200

    docs: announce v6 beta

---
+++ docs/.vitepress/theme/BetaBanner.vue
@@ -0,0 +1,8 @@
+<template>
+  <aside class="beta-banner" aria-label="Adonis Attachment v6 beta">
+    <p>
+      Adonis Attachment v6 is available in beta.
+      <a href="https://next.adonis-attachment.jrmc.dev/">Explore the documentation <span aria-hidden="true">→</span></a>
+    </p>
+  </aside>
+</template>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import BetaBanner from './BetaBanner.vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout: () => h(DefaultTheme.Layout, null, {
+    'layout-top': () => h(BetaBanner),
+  }),
+}
